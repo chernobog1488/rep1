565b6c56fbe36e9e5be60d5f0b2ee50b023476ea
chernobog1488 <revmonstra3@gmail.com>
2025-11-17 23:06:36 +0500

    Add files via upload

--- flask_project/app/routes.py
+++ flask_project/app/routes.py
@@ -1,4 +1,18 @@
+from flask import render_template
 from app import app
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 @app.route("/hello")
 def hello():
